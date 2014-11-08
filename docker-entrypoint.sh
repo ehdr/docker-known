63dd742e45ab326e629815638ba407434e235f8c
Eric Hansander <ehdr@erichansander.se>
2014-11-08 17:39:10 +0100

    Add a wait loop to wait for the MySQL server to start
    The mysql_install_db script that runs the first time you run the mysql
    container is pretty slow (10 seconds or so), so if you were too fast
    in starting this container (e.g. if you tried starting all containers
    using Fig) it would fail to connect to the MySQL server.
    
    Thus we have to retry until MySQL is up, by borrowing a wait loop from
    tutum-docker-mysql.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,12 +8,14 @@
 
 set -e
 
+me=`basename -- "$0"`
+
+MYSQL_HOSTNAME=mysql
+
 if [ -z "$MYSQL_DATABASE" -o -z "$MYSQL_USER" -o -z "$MYSQL_PASSWORD" ]; then
-    echo >&2 `basename -- "$0"` \
-        ": error: The following environment variables must be set:"
-    echo >&2 "    \$MYSQL_DATABASE, \$MYSQL_USER, \$MYSQL_PASSWORD"
-    echo >&2 "Add them as command line options to docker run:"
-    echo >&2 "    -e MYSQL_DATABASE=... -e MYSQL_USER=... -e MYSQL_PASSWORD=..."
+    echo >&2 "$me: error: The following environment variables must be set:"
+    echo >&2 "$me:      \$MYSQL_DATABASE, \$MYSQL_USER, \$MYSQL_PASSWORD"
+    echo >&2 "$me:   Add them using the -e option to docker run"
     exit 1
 fi
 
@@ -28,10 +30,30 @@ echo "dbpass = '$MYSQL_PASSWORD'" >> /var/www/known/config.ini
 chown -R root:www-data /var/www/known/uploads
 chmod -R 775 /var/www/known/uploads
 
+# The MySQL server is slow to start the very first time you run the mysql
+# container, so we have to wait.
+# Borrowed from tutum-docker-mysql on GitHub!
+LOOP_LIMIT=12
+for (( i=0 ; ; i++ )); do
+    mysql -h $MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD -e "status" > \
+        /dev/null 2>&1 \
+        && break
+    if [ ${i} -eq ${LOOP_LIMIT} ]; then
+        echo >&2 "$me: error: Timed out waiting for MySQL service to start"
+        echo >&2 "$me:   Double check that you specified the correct MySQL"
+        echo >&2 "$me:   user name and password, and that you --link'ed"
+        echo >&2 "$me:   the MySQL container using alias: $MYSQL_HOSTNAME"
+        exit 2
+    fi
+    echo "$me: Waiting for MySQL service to start... ($((i*5))/$((LOOP_LIMIT*5)) seconds)"
+    sleep 5
+done
+echo "$me: Successfully connected to MySQL service"
+
 # We could not init the DB in the Dockerfile, since then we were not yet
 # --link'ed to the MySQL container, so do it now. This is idempotent, so it's
 # ok to do it every time we run a container even though the DB is in a volume.
-mysql -h mysql -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE \
+mysql -h $MYSQL_HOSTNAME -u$MYSQL_USER -p$MYSQL_PASSWORD $MYSQL_DATABASE \
     < /var/www/known/schemas/mysql/mysql.sql
 
 source /etc/apache2/envvars
